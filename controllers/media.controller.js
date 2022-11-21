28b316288ac8cd6fc1219b4127b7186211276c25
Bryan Aulya Achmad Istighfara <bryanachmad08@gmail.com>
2022-11-21 22:17:26 +0700

    fixing result get media

--- controllers/media.controller.js
+++ controllers/media.controller.js
@@ -38,7 +38,7 @@ exports.getByKelompok = async (req, res) => {
     const { id } = req.params;
 
     try {
-        const getByKelompok = await Media.find({ no_kelompok: id }).select('-updated_at no_kelompok __v ');
+        const getByKelompok = await Media.find({ no_kelompok: id }).select('_id link created_at');
         if (!getByKelompok) {
             return jsonResponse.error(
                 req,
